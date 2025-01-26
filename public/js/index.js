1a5db9038e8862aeecdf3a4a226f903dbbe64a1b
Maahi Batra <maahi.batra@gmail.com>
2025-01-26 21:38:29 +0530

    added localstorage for welcome toggle

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,33 @@
-// let i = 0;
-// const steps = [
-//     { title: "Know Before Ye Go!", text: "We be a young crew still findin our way, so for now, we're stickin' to teachin' the basics of JavaScript. If ye were lookin' for another language, ye might need to set sail to a different shore—for now!" },
-//     { title: "The Map!", text: "This site's got articles, quizzes, and coding challenges to keep ye sharp! If ye ever get stuck, fear not—our hints will be there to lend ye a hand and guide ye back on course!" }
-// ]
+let i = 0;
 
-// next.onclick = function() {
-//     if(i === 2) {
-//         background.style.display = "none";
-//     }
+if(localStorage.getItem("welcomeToggle") === "true") {
+    background.style.display = "none";
+}
 
-//     for(let j = 0; j < steps.length; j++) {
-//         if(i === j) {
-//             title.innerText = steps[j].title;
-//             text.innerText = steps[j].text;
-//             i++;
-//             break;
-//         }
-//     }
-// }
+const steps = [
+    { title: "Know Before Ye Go!", text: "We be a young crew still findin our way, so for now, we're stickin' to teachin' the basics of JavaScript. If ye were lookin' for another language, ye might need to set sail to a different shore—for now!" },
+    { title: "The Map!", text: "This site's got articles, quizzes, and coding challenges to keep ye sharp! If ye ever get stuck, fear not—our hints will be there to lend ye a hand and guide ye back on course!" }
+];
+
+next.onclick = function() {
+    if(i === 2) {
+        background.style.display = "none";
+
+        localStorage.setItem("welcomeToggle", true);
+    }
+
+    for(let j = 0; j < steps.length; j++) {
+        if(i === j) {
+            title.innerText = steps[j].title;
+            text.innerText = steps[j].text;
+            i++;
+            break;
+        }
+    }
+}
+
+welcomeButton.onclick = function() {
+    background.style.display = "block";
+    i = 0;
+    localStorage.setItem("welcomeToggle", false);
+}
